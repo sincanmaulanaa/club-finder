e4c676d7e56978d02b1fa4e5cf37ac2ac52a4f55
sincanmaulanaa <sincanmaulanaa@gmail.com>
2022-09-16 01:22:24 +0800

    Refactor chaining promise to async/await

--- app.js
+++ app.js
@@ -340,55 +340,55 @@ document.addEventListener("DOMContentLoaded", main);
 // const makeCoffee = new Promise(executorFunction);
 // makeCoffee.then(handlerSuccess).catch(handlerError);
 
-const state = {
-  isCoffeeMakerReady: true,
-  seedStocks: {
-    arabica: 250,
-    robusta: 60,
-    liberica: 80,
-  },
-};
+// const state = {
+//   isCoffeeMakerReady: true,
+//   seedStocks: {
+//     arabica: 250,
+//     robusta: 60,
+//     liberica: 80,
+//   },
+// };
 
-const getSeeds = (type, miligrams) => {
-  return new Promise((resolve, reject) => {
-    if (state.seedStocks[type] >= miligrams) {
-      state.seedStocks[type] -= miligrams;
-      resolve("Biji kopi didapatkan!");
-    } else {
-      reject("Maaf stock kopi habis!");
-    }
-  });
-};
+// const getSeeds = (type, miligrams) => {
+//   return new Promise((resolve, reject) => {
+//     if (state.seedStocks[type] >= miligrams) {
+//       state.seedStocks[type] -= miligrams;
+//       resolve("Biji kopi didapatkan!");
+//     } else {
+//       reject("Maaf stock kopi habis!");
+//     }
+//   });
+// };
 
-const makeCoffee = (seeds) => {
-  return new Promise((resolve, reject) => {
-    if (state.isCoffeeMakerReady) {
-      resolve("Kopi berhasil dibuat!");
-    } else {
-      reject("Maaf mesin tidak dapat digunakan!");
-    }
-  });
-};
+// const makeCoffee = (seeds) => {
+//   return new Promise((resolve, reject) => {
+//     if (state.isCoffeeMakerReady) {
+//       resolve("Kopi berhasil dibuat!");
+//     } else {
+//       reject("Maaf mesin tidak dapat digunakan!");
+//     }
+//   });
+// };
 
-const servingToTable = (coffee) => {
-  return new Promise((resolve) => {
-    resolve("Pesanan kopi sudah selesai!");
-  });
-};
+// const servingToTable = (coffee) => {
+//   return new Promise((resolve) => {
+//     resolve("Pesanan kopi sudah selesai!");
+//   });
+// };
 
-const reservedCoffee = (type, miligrams) => {
-  try {
-    const seeds = getSeeds(type, miligrams);
-    const coffee = makeCoffee(seeds);
-    const result = servingToTable(coffee);
+// const reservedCoffee = (type, miligrams) => {
+//   try {
+//     const seeds = getSeeds(type, miligrams);
+//     const coffee = makeCoffee(seeds);
+//     const result = servingToTable(coffee);
 
-    console.log(result);
-  } catch (rejectedReason) {
-    console.log(rejectedReasion);
-  }
-};
+//     console.log(result);
+//   } catch (rejectedReason) {
+//     console.log(rejectedReasion);
+//   }
+// };
 
-reservedCoffee("robusta", 50);
+// reservedCoffee("robusta", 50);
 
 // PROMISE ALL
 // const arabicaOrder = () => {
