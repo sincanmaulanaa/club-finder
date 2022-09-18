0f7bca60b2134ea5fb28da7ea4b07916d2a589e4
sincanmaulanaa <sincanmaulanaa@gmail.com>
2022-09-18 14:37:18 +0800

    Remove codes

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -37,6 +37,20 @@ const main = () => {
   };
 
   buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+
+  let imageFigureElement = document.querySelector("image-figure");
+  if (!imageFigureElement) {
+    imageFigureElement = document.createElement("image-figure");
+    document.body.appendChild(imageFigureElement);
+  }
+
+  setTimeout(() => {
+    imageFigureElement.setAttribute("caption", "Gambar 1");
+  }, 1000);
+
+  setTimeout(() => {
+    imageFigureElement.remove();
+  }, 3000);
 };
 
 export default main;
