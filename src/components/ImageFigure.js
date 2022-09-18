02cb0111e42aad4ecb9aaae67d1dd7cefc67fbcd
sincanmaulanaa <sincanmaulanaa@gmail.com>
2022-09-18 15:17:01 +0800

    Remove unused methods

--- src/components/ImageFigure.js
+++ src/components/ImageFigure.js
@@ -1,28 +1,17 @@
 class ImageFigure extends HTMLElement {
-  constructor() {
-    super();
-
-    console.log("constructed");
-  }
-
   connectedCallback() {
+    this.src = this.getAttribute("src") || null;
+    this.alt = this.getAttribute("alt") || null;
+    this.caption = this.getAttribute("caption") || null;
+
+    this.innerHTML = `
+	<figure>
+		<img src="${this.src}" alt="${this.alt}"/>
+		<figcaption>${this.caption}</figcaption>
+	</figure>
+	`;
+
     console.log("connected");
-  }
-
-  disconnectedCallback() {
-    console.log("disconnected");
-  }
-
-  adoptedCallback() {
-    console.log("adopted");
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    console.log(`Attribute ${name} changed!`);
-  }
-
-  static get observedAttributes() {
-    return ["caption", "source"];
   }
 }
 
