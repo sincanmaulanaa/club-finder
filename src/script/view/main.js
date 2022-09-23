b8aee8163a04660153c3fb3bb551c5cfd467bcb9
sincanmaulanaa <sincanmaulanaa@gmail.com>
2022-09-24 01:28:27 +0800

    Refactor with more object oriented programming

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,9 +1,10 @@
 import DataSource from "../data/data-source.js";
+import "../components/SearchBar.js";
+import "../components/ClubList.js";
 
 const main = () => {
-  const searchElement = document.querySelector("#searchElement");
-  const buttonSearchElement = document.querySelector("#searchButtonElement");
-  const clubListElement = document.querySelector("#clubList");
+  const searchElement = document.querySelector("search-bar");
+  const clubListElement = document.querySelector("club-list");
 
   const onButtonSearchClicked = async () => {
     try {
@@ -15,28 +16,14 @@ const main = () => {
   };
 
   const renderResult = (results) => {
-    clubListElement.innerHTML = "";
-    results.forEach((club) => {
-      const { name, fanArt, description } = club;
-
-      const clubElement = document.createElement("div");
-      clubElement.setAttribute("class", "club");
-
-      clubElement.innerHTML = `<img class="fan-art-club" src="${fanArt}" alt="Fan Art">
-        <div class="club-info">
-          <h2>${name}</h2>
-          <p>${description}</p>
-        </div>`;
-      clubListElement.appendChild(clubElement);
-    });
+    clubListElement.clubs = results;
   };
 
   const fallbackResult = (message) => {
-    clubListElement.innerHTML = "";
-    clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+    clubListElement.renderError(message);
   };
 
-  buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+  searchElement.clickEvent = onButtonSearchClicked;
 
   let imageFigureElement = document.querySelector("image-figure");
   if (!imageFigureElement) {
