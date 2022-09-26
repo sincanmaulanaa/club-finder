dd4b201612ae2cab3ceac6d30aecf915f2b26616
sincanmaulanaa <sincanmaulanaa@gmail.com>
2022-09-26 15:02:38 +0800

    Add shadow DOM

--- src/script/components/ClubList.js
+++ src/script/components/ClubList.js
@@ -1,23 +1,38 @@
 import "./ClubItem.js";
 
 class ClubList extends HTMLElement {
+  constructor() {
+    super();
+    this._shadowDOM = this.attachShadow({ mode: "open" });
+  }
+
   set clubs(clubs) {
     this._clubs = clubs;
     this.render();
   }
 
   render() {
-    this.innerHTML = ``;
+    this._shadowDOM.innerHTML = ``;
     this._clubs.forEach((club) => {
       const clubItemElement = document.createElement("club-item");
       clubItemElement.club = club;
-      this.appendChild(clubItemElement);
+      this._shadowDOM.appendChild(clubItemElement);
     });
   }
 
   renderError(message) {
-    this.innerHTML = "";
-    this.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+    this._shadowDOM.innerHTML = `
+    <style>
+      .placeholder {
+        font-weight: lighter;
+        color: rgba(0, 0, 0, 0.5);
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+    </style>`;
+    this._shadowDOM.innerHTML += `<h2 class="placeholder">${message}</h2>`;
   }
 }
 
